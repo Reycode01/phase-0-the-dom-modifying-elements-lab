7f4e6226a8e58ae8032c84ca2b387d327eb5cc62
humphrey <writernic89@gmail.com>
2024-03-30 23:46:34 +0300

    add files

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+const mainElement = document.getElementById('main');
+
+if (mainElement) {
+    mainElement.remove();
+}
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'YOUR-NAME is the champion'; // Replace YOUR-NAME with the desired name
+
+document.body.appendChild(newHeader);
